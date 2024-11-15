b2ab1a951225416b9e97590cfd58a3cc03509b47
Nijat Kazimli <kazimli.n@pg.com>
2024-11-15 19:51:03 +0100

    add router and layout

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,27 @@
+import {
+  NavLink, Outlet,
+} from 'react-router-dom';
+import './Layout.css';
+
+function Layout() {
+  return (
+    <div>
+      <div className="navbar">
+        {/* <NavLink to="/" className="logo-link">
+          <img src={Images.logo.src} alt={Images.logo.alt} className="logo" />
+        </NavLink> logo placeholder */}
+        <div className="link-container">
+          <NavLink to="/" className="link">
+            Count
+          </NavLink>
+          <NavLink to="/history" className="link">
+            History
+          </NavLink>
+        </div>
+      </div>
+      <Outlet />
+    </div>
+  );
+}
+
+export default Layout;
---
+++ src/router.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import Layout from './components/Layout';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+    ],
+  },
+]);
+
+export default router;
